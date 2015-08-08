9fb1655d32b4d82cd9c29aa71bdf279ce65bd9d7
kgegner <gegner2@illinois.edu>
2015-08-07 21:58:18 -0600

    add comments to getdata

--- getdata.R
+++ getdata.R
@@ -1,7 +1,12 @@
 getdata <- function() {
-  # Read data
+  
+  # Specify text file to be read and its path
   txt_file <- "~/Documents/Programming/R/coursera/exploratorydata/data/household_power_consumption.txt"
+  
+  # Specify the type of data in each column
   col_classes <- c(rep("character",2), rep("numeric",7))
+  
+  # Read from the text file and create new data frame
   pwr_data <- read.table(txt_file, header = TRUE, sep = ";", colClasses = col_classes, na.strings = "?")
   
   # Subset data to include only Feb 1, 2007 through Feb 2, 2007
